c12f56dbe48379fe12da9afc5c917e0991b35855
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-01-19 17:03:51 +0100

    add toolbar of library in app

---
+++ app/src/main/java/com/naimdridi/mylibrary/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.naimdridi.mylibrary
+
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import com.naimdridi.my_library_second.Interfaces.ToolbarActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : ToolbarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbarToLoad(toolbar as Toolbar)
+    }
+}
